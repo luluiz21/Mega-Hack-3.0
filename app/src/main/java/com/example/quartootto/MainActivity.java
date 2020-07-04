c0f29fce3a67ec1d4a44574d2283e8300ce658b9
luluiz21 <luluiz21@hotmail.com>
2020-07-04 13:06:50 -0300

    alguns detalhes slide

--- app/src/main/java/com/example/quartootto/MainActivity.java
+++ app/src/main/java/com/example/quartootto/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.quartootto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
@@ -12,6 +16,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     View chao;
+    private SlidrInterface slidr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
         chao.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
 
     }
+
+    public void quartoOttoDireita(View view){
+        Intent intent = new Intent(this,QuartoOttoDireita.class);
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/quartootto/QuartoOttoDireita.java
+++ app/src/main/java/com/example/quartootto/QuartoOttoDireita.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
+
 public class QuartoOttoDireita extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quarto_otto_direita);
+
     }
 }
