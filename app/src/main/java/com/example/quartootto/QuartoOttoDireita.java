d5054a7784dc10033fc7a61dcc4126ebd06ddde5
luluiz21 <luluiz21@hotmail.com>
2020-07-04 02:09:53 -0300

    Telas do Quarto

---
+++ app/src/main/java/com/example/quartootto/QuartoOttoDireita.java
@@ -0,0 +1,14 @@
+package com.example.quartootto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuartoOttoDireita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quarto_otto_direita);
+    }
+}
---
+++ app/src/main/java/com/example/quartootto/QuartoOttoEsquerda.java
@@ -0,0 +1,14 @@
+package com.example.quartootto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuartoOttoEsquerda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
