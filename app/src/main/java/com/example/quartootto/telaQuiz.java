7c9dbc57fd055412ee3aa4470975a90889bded2d
luluiz21 <luluiz21@hotmail.com>
2020-07-04 02:44:45 -0300

    esboço tela Quiz

---
+++ app/src/main/java/com/example/quartootto/telaQuiz.java
@@ -0,0 +1,14 @@
+package com.example.quartootto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_quiz);
+    }
+}
