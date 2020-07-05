0204333609b46a59b6cf8a110c518094c21a4cb4
luluiz21 <luluiz21@hotmail.com>
2020-07-05 07:12:41 -0300

    últimas telas concluidas

--- app/src/main/java/com/example/quartootto/mapaDoBrasil.java
+++ app/src/main/java/com/example/quartootto/mapaDoBrasil.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class mapaDoBrasil extends AppCompatActivity {
 
@@ -14,14 +15,26 @@ public class mapaDoBrasil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa_do_brasil);
         regiaoNordeste();
+        regiaoNorte();
 
     }
     public void regiaoNordeste(){
-        Button nordeste = findViewById(R.id.botaoNordeste);
+        TextView nordeste = findViewById(R.id.NordesteMapa);
         nordeste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(mapaDoBrasil.this,Regiao.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void regiaoNorte(){
+        TextView norte = findViewById(R.id.NorteMapa);
+        norte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mapaDoBrasil.this,Norte.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/quartootto/telaQuiz.java
+++ app/src/main/java/com/example/quartootto/telaQuiz.java
@@ -1,11 +1,14 @@
 package com.example.quartootto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,9 +18,76 @@ public class telaQuiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_quiz);
-        iniciarQuiz();
+        voltarQuarto();
+        botaoErrado1();
+        botaoCerto1();
+        botaoErrado2();
+        botaoCerto2();
     }
-    private void iniciarQuiz(){
+
+    private void botaoCerto2() {
+        Button certo = findViewById(R.id.opcao4);
+        certo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                proximaPergunta();
+            }
+        });
+    }
+
+    private void botaoErrado2() {
+        Button errado = findViewById(R.id.opcao3);
+        errado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                desejaContinuar();
+            }
+        });
+    }
+
+    private void botaoCerto1() {
+        Button certo = findViewById(R.id.opcao2);
+        certo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                proximaPergunta();
+            }
+        });
+    }
+
+    private void proximaPergunta() {
+        AlertDialog.Builder msgBox = new AlertDialog.Builder(this);
+        msgBox.setTitle("Você Acertou! :)");
+        msgBox.setMessage("Deseja continuar, com outra pergunta?");
+        msgBox.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(telaQuiz.this,"Boa Sorte!",Toast.LENGTH_SHORT).show();
+                Button pergunta = findViewById(R.id.pergunta);
+                Button opcao1 = findViewById(R.id.opcao1);
+                Button opcao2 = findViewById(R.id.opcao2);
+                Button opcao3 = findViewById(R.id.opcao3);
+                Button opcao4 = findViewById(R.id.opcao4);
+                pergunta.setText("Quem é o chefe dos Capitães da Areia?");
+                opcao1.setText("Almiro");
+                opcao2.setText("Pirulito");
+                opcao3.setText("Gato");
+                opcao4.setText("Pedro-Bala");
+
+            }
+        });
+        msgBox.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(telaQuiz.this,"Aguardo seu retorno!",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(telaQuiz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        msgBox.show();
+    }
+
+    private void voltarQuarto(){
         FloatingActionButton botaoVoltar = findViewById(R.id.botaoVoltarTelaInicial);
         botaoVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +96,35 @@ public class telaQuiz extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void botaoErrado1(){
+        Button errado = findViewById(R.id.opcao1);
+        errado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                desejaContinuar();
+            }
+        });
+    }
+
+    private void desejaContinuar() {
+        AlertDialog.Builder msgBox = new AlertDialog.Builder(this);
+        msgBox.setTitle("Você errou! :(");
+        msgBox.setMessage("Deseja tentar mais uma vez?");
+        msgBox.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(telaQuiz.this,"Boa Sorte!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        msgBox.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(telaQuiz.this,"Boa sorte da próxima!",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(telaQuiz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        msgBox.show();
     }
 }
